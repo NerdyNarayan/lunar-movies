0e7388b9b79f28424a5e9606ef403b73a533e1cc
NerdyNarayan <nerdynarayan@gmail.com>
2024-07-27 16:11:20 +0530

    feat: init using my next starter

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import ThemeToggle from "@/components/theme-toggle";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
+};
+
+export default Page;
